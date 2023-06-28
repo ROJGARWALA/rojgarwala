be3992245fa51b0726bb796aa1570b2635566e9b
Shashank Mungantiwar <srmungantiwar1@gmail.com>
2023-06-29 00:19:19 +0530

    Initial commit

---
+++ src/main/java/com/app/rojgarwala/RojgarwalaApplication.java
@@ -0,0 +1,13 @@
+package com.app.rojgarwala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojgarwalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RojgarwalaApplication.class, args);
+	}
+
+}
